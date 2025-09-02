c084157584a50753bc566652495ab36dd509bacc
Abhinav Singh <95268334+abhinavsrepository@users.noreply.github.com>
2025-09-03 02:08:57 +0530

    initial

---
+++ ecogram-pos-system/src/main/java/com/pos/configuration/JwtConstant.java
@@ -0,0 +1,7 @@
+package com.pos.configuration;
+
+public class JwtConstant {
+
+    public static final String JWT_SECRET = "qwertyuiopasdfghjkzxcvbnmasdfghjkl1";
+    public static final String JWT_HEADER = "Authorization";
+}
--- ecogram-pos-system/src/main/java/com/pos/configuration/JwtValidator.java
+++ ecogram-pos-system/src/main/java/com/pos/configuration/JwtValidator.java
@@ -1,5 +1,9 @@
 package com.pos.configuration;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import io.jsonwebtoken.io.IOException;
@@ -8,11 +12,34 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
+import javax.crypto.SecretKey;
+
 public class JwtValidator extends OncePerRequestFilter {
 
     @Override
-    protected void doFilterInternal (HttpServletRequest request, HttpServletResponse response,
-    FilterChain filterChain) throws ServletException,IOException{
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+
+        String jwt = request.getHeader(JwtConstant.JWT_HEADER);
+        //Bearer jwt
+        if (jwt!=null){
+            jwt = jwt.substring(7);
+
+            try {
+                SecretKey key = Keys.hmacShaKeyFor(JwtConstant.JWT_SECRET.getBytes());
+                Claims claims = Jwts.parser()
+                        .verifyWith(key)
+                        .build()
+                        .parseSignedClaims(jwt)
+                        .getPayload();
+
+                String email = String.valueOf(claims.get("email"));
+                String authorities = String.valueOf(claims.get("authorities"));
+            }
+            catch (Exception e) {
+            }
+        }
 
     }
 
